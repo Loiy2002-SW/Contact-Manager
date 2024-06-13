57d6cc00e6363e9b23ad414959abdd1309337711
loiy <lloiy9310@gmail.com>
2024-06-13 11:15:02 +0300

    ADD the necessary method to the program

--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace ContactManager
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+
+        //Add contact method
+        public static string[] AddContact()
+        {
+
+            return new string[] { };
+        }
+
+        //Remove contact method
+        public static string[] RemoveContact()
+        {
+
+            return new string[] { };
+        }
+
+        //View All Contacts method
+        public static string[] ViewAllContacts()
+        {
+
+            return new string[] { };
         }
     }
 }
---
+++ ContactManagerTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ContactManagerTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
